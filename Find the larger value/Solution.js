1da68fbf4f170ef8fc6d285bf73dff8395323843
Harrshini B <72803388+harrshini12@users.noreply.github.com>
2020-12-08 15:54:53 +0100

    Solution - Find the largest value

---
+++ Find the larger value/Solution.js
@@ -0,0 +1,28 @@
+//Ex: 1
+tomHeight = 180;
+samHeight = 160;
+if(tomHeight > samHeight)
+    {
+        console.log('The tall boy is Tom with the height of ' + tomHeight + ' cm');
+    }else
+    {
+         console.log('The tall boy is Sam with the height of ' + samHeight + ' cm');
+	}
+	
+//Ex: 2  
+num1 = 20;
+num2 = 30;
+num3 = 40;
+    if(num1>num2 && num1>num3)
+		{
+			console.log('The largest number is ' + num1);
+		}
+	else if(num2>num1 && num2>num3)
+		{
+			console.log('The largest number is ' + num2);
+		}
+	else if(num3>num1 && num3>num1)
+		{
+			console.log('The largest number is ' + num3);
+		}
+		
