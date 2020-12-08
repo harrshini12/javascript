7616739f178cd71fb6473d8b120b0b0382800693
Harrshini B <72803388+harrshini12@users.noreply.github.com>
2020-12-08 16:25:11 +0100

    Solution - Guessing Game

---
+++ Play a guessing game with Sally/chatbot.js
@@ -0,0 +1,25 @@
+readlineSync = require('readline-sync')
+let botName = "Sally"
+let randomNumber = "78";
+console.log("Hi! My name is " + botName + ". I am thinking about a number. Try to guess it! "); 
+i = 0;
+//while(i >= 0){
+while(true){
+    let userNumber = readlineSync.question("Input the number ");
+    if(userNumber === randomNumber){
+        console.log("Great job! You guessed the "+ randomNumber + " that I thought of.");
+        break;
+    }else if(userNumber < randomNumber){
+        console.log("My number is larger!");
+    }else if(userNumber > randomNumber){
+        console.log("My number is smaller!");
+    }
+    i++;
+}
+console.log("nice job! you guessed after " + i + " tries")
+
+
+
+
+
+
