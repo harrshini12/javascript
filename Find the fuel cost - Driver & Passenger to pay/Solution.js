3dbce9fd40b42f812238eb1ba97a08a4964e51ae
Harrshini B <72803388+harrshini12@users.noreply.github.com>
2020-12-08 15:26:42 +0100

    Solution

---
+++ Find the fuel cost - Driver & Passenger to pay/Solution.js
@@ -0,0 +1,16 @@
+let totalGasCost = 189;
+let passengers = 5;
+let driverDiscount = 15;
+
+// This formula we find by thinking about the problem mathematically (pen & paper)
+let passengerCost = (totalGasCost + driverDiscount)/passengers;
+let driverCost = passengerCost - driverDiscount;
+
+console.log(passengerCost);
+console.log(driverCost);
+// should be 15
+console.log(passengerCost - driverCost);
+
+// should be equal to totalGasCost
+let totalSum = 4 * passengerCost + driverCost;
+console.log(totalSum);
